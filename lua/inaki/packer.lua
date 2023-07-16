099061324b34b88c8782121901f9b8a159413c39
Iñaki Pujol <ipujol10@gmail.com>
2023-07-16 10:21:10 +0200

    DEL: autoclose

--- lua/inaki/packer.lua
+++ lua/inaki/packer.lua
@@ -72,6 +72,5 @@ return require('packer').startup(function(use)
     use("folke/zen-mode.nvim")
     --  use("github/copilot.vim") -- Needs subscription
     use("laytan/cloak.nvim")
-    use('m4xshen/autoclose.nvim')
 
 end)
